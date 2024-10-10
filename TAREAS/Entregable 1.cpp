b47738e16b1bd93367b644b2d6d242ddebc645bc
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2024-10-10 14:12:47 -0600

    Update Entregable 1.cpp

--- TAREAS/Entregable 1.cpp
+++ TAREAS/Entregable 1.cpp
@@ -1,65 +1,125 @@
-#include <iostream>
-
 using namespace std;
 
 int main()
 {
-	// 1 Declare una variable tipo int que tenga el número de materias que estás cursando actualmente.
+	
+		// 1 Declare una variable tipo int que tenga el número de materias que estás cursando actualmente.
 
-	int numMaterias = 6;
-	cout << "Tengo: " << numMaterias << " materias" << endl;
+		int numMaterias = 6;
+		cout << "Tengo: " << numMaterias << " materias" << endl;
 
-	// 2 Declare una variable tipo int que guarde el valor del cuatrimestre que estas cursando.
-	int numCuatrimestre = 5;
-	cout << "Estoy en: " << numCuatrimestre << "to cuatrimestre" << endl;
+		// 2 Declare una variable tipo int que guarde el valor del cuatrimestre que estas cursando.
+		int numCuatrimestre = 5;
+		cout << "Estoy en: " << numCuatrimestre << "to cuatrimestre" << endl;
 
-	// 3 Declara una variable tipo string con identificador “materia”, e inicializar con el nombre de esta materia.
-	string materia = "Fundamentos de programacion";
-	cout << "Esta materia es: " << materia << endl;
+		// 3 Declara una variable tipo string con identificador “materia”, e inicializar con el nombre de esta materia.
+		string materia = "Fundamentos de programacion";
+		cout << "Esta materia es: " << materia << endl;
 
-	// 4 Declara una variable tipo string con identificador “cuenta”, e inicializar con el tu número de cuenta / matricula del Unitec.
-	string cuenta = "335002365";
-	cout << "Mi numero de cuenta es: " << cuenta << endl;
+		// 4 Declara una variable tipo string con identificador “cuenta”, e inicializar con el tu número de cuenta / matricula del Unitec.
+		string cuenta = "335002365";
+		cout << "Mi numero de cuenta es: " << cuenta << endl;
 
-	// 5 Declara una variable de tipo string con identificador “nombre”, e inicializar con tu nombre(s) propio(s).
-	string nombres = "Patricio Joel";
-	cout << "Mi nombre es: " << nombres << endl;
+		// 5 Declara una variable de tipo string con identificador “nombre”, e inicializar con tu nombre(s) propio(s).
+		string nombres = "Patricio Joel";
+		cout << "Mi nombre es: " << nombres << endl;
 
-	// 6 Declara una variable de tipo string con identificador “apellidoPaterno”, e inicializar con tu apellido paterno.
-	string apellidoPaterno = "Hernandez";
-	cout << "Mi primer apellido es: " << apellidoPaterno << endl;
+		// 6 Declara una variable de tipo string con identificador “apellidoPaterno”, e inicializar con tu apellido paterno.
+		string apellidoPaterno = "Hernandez";
+		cout << "Mi primer apellido es: " << apellidoPaterno << endl;
 
-	// 7 Declara una variable de tipo string con identificador “apellidoMaterno”, e inicializar con tu apellido materno.
-	string apellidoMaterno = "Garcia";
-	cout << "Mi segundo apellido es: " << apellidoMaterno << endl;
+		// 7 Declara una variable de tipo string con identificador “apellidoMaterno”, e inicializar con tu apellido materno.
+		string apellidoMaterno = "Garcia";
+		cout << "Mi segundo apellido es: " << apellidoMaterno << endl;
 
-	// 8 Declara una variable tipo long con identificador “celular”, e inicializar con tu número celular.
-	long int celular = 5513598636;
-	cout << "Mi numero de celular es: " << celular << endl;
+		// 8 Declara una variable tipo long con identificador “celular”, e inicializar con tu número celular.
+		long int celular = 5513598636;
+		cout << "Mi numero de celular es: " << celular << endl;
 
-	// 9 Declara una variable tipo long con identificador “casa”, e inicializar con tu número de casa.
-	long int numCasa = 228;
-	cout << "Mi numero de casa es: " << numCasa << endl;
+		// 9 Declara una variable tipo long con identificador “casa”, e inicializar con tu número de casa.
+		long int numCasa = 228;
+		cout << "Mi numero de casa es: " << numCasa << endl;
 
-	// 10 Declara una variable tipo string con identificador “correoQueMasUso”, e inicializar con el correo electrónico que más uses.
-	string correoQueMasUso = "hernandezpatricio.167@gmail.com";
-	cout << "El correo que mas uso es: " << correoQueMasUso << endl;
+		// 10 Declara una variable tipo string con identificador “correoQueMasUso”, e inicializar con el correo electrónico que más uses.
+		string correoQueMasUso = "hernandezpatricio.167@gmail.com";
+		cout << "El correo que mas uso es: " << correoQueMasUso << endl;
 
-	// 11 Declara una variable tipo string con identificador “correMyUnitec”, e inicializar con el correo electrónico que tienes asignado en el Unitec.
-	string correoMyUnitec = "patricio.hernandez35@my.unitec.edu.mx";
-	cout << "Mi correo de unitec es: " << correoMyUnitec << endl;
+		// 11 Declara una variable tipo string con identificador “correMyUnitec”, e inicializar con el correo electrónico que tienes asignado en el Unitec.
+		string correoMyUnitec = "patricio.hernandez35@my.unitec.edu.mx";
+		cout << "Mi correo de unitec es: " << correoMyUnitec << endl;
 
-	// 12 Declara una variable tipo string con identificador “carrera”, e inicializar con la carrera que cursas actualmente en el Unitec.
-	string carrera = "Diseno, animacion y arte digital";
-	cout << "La carrera que estoy cursando es: " << carrera << endl;
+		// 12 Declara una variable tipo string con identificador “carrera”, e inicializar con la carrera que cursas actualmente en el Unitec.
+		string carrera = "Diseno, animacion y arte digital";
+		cout << "La carrera que estoy cursando es: " << carrera << endl;
 
-	// 13 Declara una variable tipo bool con identificador “blackboard”, e inicializar con valor true, si lo has usado anteriormente, de lo contrario inicializar con valor false.
-	bool blackboard = true;
-	cout << blackboard;
+		// 13 Declara una variable tipo bool con identificador “blackboard”, e inicializar con valor true, si lo has usado anteriormente, de lo contrario inicializar con valor false.
+		bool blackboard = true;
+		cout << blackboard;
 
-	// Errores de sintaxis 
+		// ERRORES DE SINTAXIS
 
-    int numero = 0; 
+		// Ejercicio 1
+		int numero = 5; // no acepta numero
+
+		// Ejercicio 2
+		float valor = 3.14;  //  no reconoce el caracteres especiales como el @
+
+		// Ejercicio 3
+		double casa = 9.81; // en lugar de doble tipo de variable falta la variable
+
+		// Ejercicio 4
+		char letra = 'AB'; // no hay error
+
+		// Ejercicio 5
+		bool esVerdadero = "true"; // no hay error
+
+		// Ejercicio 6
+		int numero = 10.5;  // no hay error
+
+		// Ejercicio 7
+		string nombre = "Juan";  // en lugar de comillas simples un string lleva comillas dobles
+
+		// Ejercicio 8
+		const int PI = 3.14; // no hay error
+
+		// Ejercicio 9
+		int suma = 5 + 9; // falta completar la suma con el segundo digito
+
+		// Ejercicio 10
+		int numero = 8;  
+		int numero = 10;  // Se puede corregir añadiendo tipo de variable o completando el valor de la primer variable
+
+		// Ejercicio 11
+		string numero = "diez";  // cuando es texto va con tipo string o bien si es int es un valor numérico
+
+		// Ejercicio 12
+		float altura = 1.75;  // en lugar de coma, para numeros decimales se usa punto y sin espacios
+
+		// Ejercicio 13
+		char inicial = 'j';  // cuando es de tipo char lleva comillas simples 
+
+		// Ejercicio 14
+		bool esMayor = 1;  // no hay error
+
+		// Ejercicio 15 
+		string apellido = "Juan";  // cuando es tipo string lleva comillas dobles
+
+		// Ejercicio 16
+		int resultado = 5 * 8;  // falta completar la operacion con el segundo digito
+
+		// Ejercicio 17
+		double radio = 5.55;
+		double area = 3.14 * radio * radio;  // falto especificar radio y la manera de elevar esta mal 
+
+		// Ejercicio 18
+		int edad = 25;  // hace falta el ;
+
+		// Ejercicio 19
+		float peso = 70.5f;  // no hay error
+
+		// Ejercicio 20
+		int suma = 10 + 20;  // hace falta el ; 
 
 }
+
 
