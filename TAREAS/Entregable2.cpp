ca6d320205917973d27793ee4ce86b8d27f0de8b
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2024-10-17 12:42:49 -0600

    Create Entregable2.cpp

---
+++ TAREAS/Entregable2.cpp
@@ -0,0 +1,24 @@
+// Ejercicio 1
+double Resultado1 = 3 + 5 * 2 / 2 - 1;
+double Resultado2 = 3 + 10 / 2 - 1;
+double Resultado3 = 3 + 5 - 1;
+double Resultado4 = 8 - 1;
+double Resultado5 = 1 / 3 * 3;
+
+cout << "Resultado de 3 + 5 * 2 / 2 - 1: " << Resultado1 << endl;
+cout << "Resultado de 3 + 10 / 2 - 1: " << Resultado2 << endl;
+cout << "Resultado de 3 + 5 - 1: " << Resultado3 << endl;
+cout << "Resultado de 8 - 1: " << Resultado4 << endl;
+cout << "Resultado de 1/3 * 3: " << Resultado5 << endl;
+
+// Ejercicio 2
+int numero;
+cout << "Ingrese un número: ";
+cin >> numero;
+
+int potencia = numero * numero; // Calcula la potencia al cuadrado
+cout << "La potencia de " << numero << " es " << potencia << endl;
+
+// Ejercicio 3
+int yardas;
+cout << "ingrese numero de yardas: ";
