e2b673d40c377bd8b3432b62e5ff4763b9771232
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2024-11-04 12:07:20 -0600

    Create notasEntregable4.cpp

---
+++ programacion /PARCIAL2/notasEntregable4.cpp
@@ -0,0 +1,43 @@
+    int A, B, C;
+    A = 20;
+    B = 70;
+    C = 40;
+
+    if (B >= A and B >= C)
+    {
+        cout << "B es mas grande " << B << endl;
+    }
+
+    if (C >= A and C >= B)
+    {
+        cout << "C es mas grande " << C << endl;
+    }
+
+    if (A >= B and A >= C)
+    {
+        cout << "A es mas grande " << A << endl;
+    }
+
+    //2
+
+     A, B, C;
+
+
+    if (A <= B and A <= C)
+    {
+        cout << "A es mas chico " << A << endl;
+    }
+
+    if (B <= A and B <= C)
+    {
+        cout << "B es mas chico " << B << endl;
+    }
+
+    if (C <= A and C <= B)
+    {
+        cout << "C es mas chico " << C << endl;
+    }
+
+    // se puede juntar en el mismo ejercicio el maximo y minimo.
+
+}
