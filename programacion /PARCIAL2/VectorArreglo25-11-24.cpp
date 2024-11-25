ff7700fed3da2e9fbeadcb3c924b8388aa779210
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2024-11-25 11:44:12 -0600

    Update VectorArreglo25-11-24.cpp

--- programacion /PARCIAL2/VectorArreglo25-11-24.cpp
+++ programacion /PARCIAL2/VectorArreglo25-11-24.cpp
@@ -17,4 +17,20 @@ while (num != 0)
     {
         cout << elemento << endl;
     }
+
+int numeros[10]
+{34, 23, 94, 56, 16, 27, 5, 25, 42, 82 };
+
+vector <int> vnum
+{ 34, 23, 94, 56, 16, 27, 5, 25, 42, 82 };
+
+int mayor = -1;
+for (int i = 0; i <= 9; i++)
+{
+    if (numeros[i] < mayor)
+    {
+        mayor = numeros[i];
+    }
 }
+
+cout << "el mayor es: " << mayor << endl;
