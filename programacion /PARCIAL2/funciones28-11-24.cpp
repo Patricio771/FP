5bd9431cf3f2d15041345ebaab9f227030697cb7
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2024-11-28 11:48:53 -0600

    Update funciones28-11-24.cpp

--- programacion /PARCIAL2/funciones28-11-24.cpp
+++ programacion /PARCIAL2/funciones28-11-24.cpp
@@ -23,6 +23,39 @@ int main() //Punto de partida
 {
     // hola(); Esto es invocar o llamar la funcion
     suma();
+}
+
+
+
+
+
+parte 2
+//variables globales (fuera de la funcion main)
+int n1, n2;
+//funciones
+void pedirNumeros()
+{
+    cout << "Dame el No. 1: ";
+    cin >> n1;
+    cout << "Dame el No. 2: ";
+    cin >> n2;
+}
+void hola()
+{
+    cout << "Hola mundo";
+}
+
+void suma(int n1, int n2) //variable local, tambien son los parametros de recepcion (int n1, int n2)
+{
+    //Suma de parametros.
+    cout << n1 + n2 << endl;
+}
+
+
+int main() 
+{
+    pedirNumeros();
+    suma(n1, n2);  //Estos se llaman argumentos
 
 
 }
