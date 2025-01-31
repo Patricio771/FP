ca89ba2d6d014920c0f88179faf98e814a8e486d
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2025-01-31 12:45:18 -0600

    Create vocales_Mayus.cpp

---
+++ ESTRUCTURA_DATOS/PUNTEROS/vocales_Mayus.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+void pedirDatos();
+void contarVocales(char *);
+char palabraUsuario[30];
+
+int main()
+{
+	pedirDatos();
+	contarVocales(palabraUsuario);
+}
+
+void pedirDatos()
+{
+	cout << "Digita una palabra: ";
+	cin.getline(palabraUsuario, 30, '\n');
+}
+void contarVocales(char* palabra)
+{
+	int contA = 0, contE = 0, contI = 0, contO = 0, contU = 0;
+	while (*palabra)
+	{
+		switch (*palabra)
+		{
+		case 'A': contA++; break;
+		case 'E': contE++; break;
+		case 'I': contI++; break;
+		case 'O': contO++; break;
+		case 'U': contU++; break;
+		}
+		palabra++;
+	}
+	cout << "\nNumero de vocales A: " << contA << endl;
+	cout << "Numero de vocales E: " << contE << endl;
+	cout << "Numero de vocales I: " << contI << endl;
+	cout << "Numero de vocales O: " << contO << endl;
+	cout << "Numero de vocales U: " << contU << endl;
+}
