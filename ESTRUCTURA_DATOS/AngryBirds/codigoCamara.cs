c06f66a357856046f04d612eae7a514db0a06a2c
Patricio771 <patricio.hernandez35@my.unitec.edu.mx>
2025-04-01 12:17:55 -0600

    Create codigoCamara.cs

---
+++ ESTRUCTURA_DATOS/AngryBirds/codigoCamara.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform birdTransform;
+    [SerializeField] private float xStopPosition;
+
+    private Vector3 startPosition;
+
+    //Start se ejecuta una vez antes del primer frame
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (birdTransform.position.x > transform.position.x && transform.position.x < xStopPosition)
+        {
+            transform.position = new Vector3(birdTransform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = startPosition;
+    }
+
+}
+
